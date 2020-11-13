8fc7ce40b2e55c08682f47e5d07146d7506bd2f9
avaraz <60288526+avaraz@users.noreply.github.com>
2020-11-13 17:34:29 +0100

    Add files via upload

---
+++ zad.3.14/zad.3.14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace zad._3._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Przedzial od 1 do n. Podaj N: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int suma = 0;
+                for (int j = 1; j < i; j++)
+                    if (i % j == 0)
+                        suma += j;
+                if (suma == i)
+                    Console.WriteLine("Liczba {0} jest liczba doskonala.", i);
+            }
+        }
+    }
+}
