c43c87c07921743ffb2ea6080092387ed08f1d56
avaraz <60288526+avaraz@users.noreply.github.com>
2020-11-26 19:16:16 +0100

    Add files via upload

---
+++ zad.5.04/zad.5.04/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace zad._5._04
+{
+    class Program
+    {
+        static void mnozenie(int[] liczby, int x)
+        {
+            Console.WriteLine("\n Po mnożeniu:");
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                liczby[i] = liczby[i] * x;
+                Console.Write(liczby[i] + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 5 liczb:");
+            int[] liczby = new int[5];
+
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                liczby[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Podaj mnożnik:");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Przed mnożeniem:");
+            for (int i = 0; i < liczby.Length; i++)
+            {
+                Console.Write(liczby[i] + " ");
+            }
+
+            mnozenie(liczby, x);
+        }
+    }
+}
---
+++ zad.5.05/zad.5.05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace zad._5._05
+{
+    class Program
+    {
+        static void rysuj(int a, int b)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj Szerokość:");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            rysuj(a,b);
+            Console.WriteLine();
+            rysuj(b, a);
+        }
+    }
+}
