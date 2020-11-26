5a9ffe2a7ed33446aa1f79f01435c7dafcbd91b0
avaraz <60288526+avaraz@users.noreply.github.com>
2020-11-26 19:47:16 +0100

    Add files via upload

---
+++ zad.5.09/zad.5.09/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace zad._5._09
+{
+    class Program
+    {
+        static int rekurencja(int n)
+        {
+            if ((n == 1) || (n == 2))
+                return 1;
+            else
+                return rekurencja(n - 1) + rekurencja(n - 2);
+        }
+
+        static int iteracja(int n)
+        {
+            int a = 0;
+            int b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                b += a;
+                a = b - a;
+            }
+            return a;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+
+            Console.WriteLine("Podaj, ktory wyraz ciagu Fibonacciego obliczyc");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} wyraz ciagu Fibonacciego: {1}", n, rekurencja(n));
+            Console.WriteLine("{0} wyraz ciagu Fibonacciego: {1}", n, iteracja(n));
+        }
+    }
+
+}
---
+++ zad.5.10/zad.5.10/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace zad._5._10
+{
+    class Program
+    {
+        static int Oblicz(int n)
+        {
+            if (n <= 1) return (1);
+            else return (n + Oblicz(n - 1));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Oblicz(5));
+        }
+    }
+}
